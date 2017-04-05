d5704a37f53310e80ac4ea3c37138f496336321f
kpuno <kylepuno@outlook.com>
2017-04-05 21:13:28 +0000

    removed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(favicon(__dirname + '/favicon.ico'));
 
 // DB Setup
 // 'mongodb://user:12345@ds141950.mlab.com:41950/what-to-cook'
-mongoose.connect(process.env.URI || 'mongodb://user:12345@ds141950.mlab.com:41950/what-to-cook');
+// mongoose.connect(process.env.URI || 'mongodb://user:12345@ds141950.mlab.com:41950/what-to-cook');
 
 // App Setup
 app.use(morgan('combined'));
